b1ff9b594cd71efc1f4394e014b60b12f77ae680
WLBF <lbf1353@live.com>
2022-07-26 19:40:34 +0800

    fill license notice

--- controller/controller.go
+++ controller/controller.go
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2022 lbf1353@live.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controller
 
 import (
@@ -8,7 +24,6 @@ import (
 	informers "k8s.io/client-go/informers/core/v1"
 	listers "k8s.io/client-go/listers/core/v1"
 	"k8s.io/client-go/tools/cache"
-	"k8s.io/klog/v2"
 	"math/rand"
 	"net"
 	"time"
